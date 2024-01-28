7d1c6612a0d3b127ec1b0fc32950f365208f823e
Cristiano Araújo <cristiano112715@gmail.com>
2024-01-28 15:23:07 -0300

    first commit

---
+++ src/componentes/Card/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+
+export default function Card(props) {
+
+  return(
+    
+    <div id="card" className={props.ehTemaEscuro ? 'card-modo-escuro' : 'card-modo-claro'}>
+      <div className="card-content">
+      <p className="primeiroParagrafo">{props.data}</p>
+      <h4>{props.titulo}</h4>
+      <p className="segundoParagrafo">{props.empresa}</p>
+      <p>{props.paragrafo}</p>
+      </div>
+    </div>
+    
+  );
+}
